9263d237db0c427d6ca83e873bccdea6b35e1bac
danielsb407 <danielsb407@gmail.com>
2024-02-29 23:49:02 -0500

    updated SPI.cs for testing

--- SPI/SPI.cs
+++ SPI/SPI.cs
@@ -5,12 +5,6 @@
 //
 // You can short MOSI to MISO for testing.
 //
-// T7:
-//     MOSI    FIO2
-//     MISO    FIO3
-//     CLK     FIO0
-//     CS      FIO1
-//
 // T4:
 //     MOSI    FIO6
 //     MISO    FIO7
@@ -54,17 +48,17 @@ namespace SPI
             int port = 0;
             int maxBytesPerMB = 0;
             string ipAddrStr = "";
-            int numBytes = 4;
-            byte []aBytes = new byte[4];
+            int numBytes = 1;
+            byte[] aBytes = new byte[1];
+            int numOBytes = 16;
+            byte[] oBytes = new byte[16];
             Random rand = new Random();
 
             try
             {
-                //Open first found LabJack
-                LJM.OpenS("ANY", "ANY", "ANY", ref handle);  // Any device, Any connection, Any identifier
-                //LJM.OpenS("T7", "ANY", "ANY", ref handle);  // T7 device, Any connection, Any identifier
-                //LJM.OpenS("T4", "ANY", "ANY", ref handle);  // T4 device, Any connection, Any identifier
-                //LJM.Open(LJM.CONSTANTS.dtANY, LJM.CONSTANTS.ctANY, "ANY", ref handle);  // Any device, Any connection, Any identifier
+                
+                LJM.OpenS("T4", "ANY", "ANY", ref handle);  // T4 device, Any connection, Any identifier
+               
 
                 LJM.GetHandleInfo(handle, ref devType, ref conType, ref serNum, ref ipAddr, ref port, ref maxBytesPerMB);
                 LJM.NumberToIP(ipAddr, ref ipAddrStr);
@@ -73,37 +67,27 @@ namespace SPI
                 Console.WriteLine("Max bytes per MB: " + maxBytesPerMB);
                 Console.Out.WriteLine("");
 
-                if (devType == LJM.CONSTANTS.dtT4)
-                {
-                    //Configure FIO4 to FIO7 as digital I/O.
-                    LJM.eWriteName(handle, "DIO_INHIBIT", 0xFFF0F);
-                    LJM.eWriteName(handle, "DIO_ANALOG_ENABLE", 0x00000);
+               //Configure FIO4 to FIO7 as digital I/O.
+                LJM.eWriteName(handle, "DIO_INHIBIT", 0xFFF0F);
+                LJM.eWriteName(handle, "DIO_ANALOG_ENABLE", 0x00000);
 
-                    //Setting CS, CLK, MISO, and MOSI lines for the T4. FIO0
-                    //to FIO3 are reserved for analog inputs, and SPI requires
-                    //digital lines.
-                    LJM.eWriteName(handle, "SPI_CS_DIONUM", 5);  //CS is FIO5
-                    LJM.eWriteName(handle, "SPI_CLK_DIONUM", 4);  //CLK is FIO4
-                    LJM.eWriteName(handle, "SPI_MISO_DIONUM", 7);  //MISO is FIO7
-                    LJM.eWriteName(handle, "SPI_MOSI_DIONUM", 6);  //MOSI is FIO6
-                }
-                else
-                {
-                    //Setting CS, CLK, MISO, and MOSI lines for the T7 and
-                    //other devices.
-                    LJM.eWriteName(handle, "SPI_CS_DIONUM", 1);  //CS is FIO1
-                    LJM.eWriteName(handle, "SPI_CLK_DIONUM", 0);  //CLK is FIO0
-                    LJM.eWriteName(handle, "SPI_MISO_DIONUM", 3);  //MISO is FIO3
-                    LJM.eWriteName(handle, "SPI_MOSI_DIONUM", 2);  //MOSI is FIO2
-                }
+                //Setting CS, CLK, MISO, and MOSI lines for the T4. FIO0
+                //to FIO3 are reserved for analog inputs, and SPI requires
+                //digital lines.
+                LJM.eWriteName(handle, "SPI_CS_DIONUM", 5);  //CS is FIO5
+                LJM.eWriteName(handle, "SPI_CLK_DIONUM", 4);  //CLK is FIO4
+                LJM.eWriteName(handle, "SPI_MISO_DIONUM", 7);  //MISO is FIO7
+                LJM.eWriteName(handle, "SPI_MOSI_DIONUM", 6);  //MOSI is FIO6
+                
+             
 
                 //Selecting Mode CPHA=1 (bit 0), CPOL=1 (bit 1)
-                LJM.eWriteName(handle, "SPI_MODE", 3);
+                LJM.eWriteName(handle, "SPI_MODE", 0);
 
                 //Speed Throttle:
                 //Valid speed throttle values are 1 to 65536 where 0 = 65536.
                 //Configuring Max. Speed (~800 kHz) = 0
-                LJM.eWriteName(handle, "SPI_SPEED_THROTTLE", 0);
+                LJM.eWriteName(handle, "SPI_SPEED_THROTTLE", 61100);
 
                 //Options
                 //bit 0:
@@ -133,40 +117,49 @@ namespace SPI
                     Console.WriteLine("  " + aNames[i] + " = " + aValues[i]);
                 }
 
-                //Write(TX)/Read(RX) 4 bytes
-                numBytes = 4;  //Redundant but to reiterate 4 TX/RX bytes
+        
+
+                //Write(TX)/Read(RX) 1 bytes
+                
                 LJM.eWriteName(handle, "SPI_NUM_BYTES", numBytes);
 
-                //Write the bytes
-                for (int i = 0; i < numBytes; i++)
-                {
-                    aBytes[i] = Convert.ToByte(rand.Next(255));
-                }
-                LJM.eWriteNameByteArray(handle, "SPI_DATA_TX", numBytes, aBytes, ref errAddr);
-                LJM.eWriteName(handle, "SPI_GO", 1);  //Do the SPI communications
+                //Set command to be sent
+                //0x00: no_op, recieve 0xA5(165) in response; send this if getting strange response to clear out buffer
+                //0x10: read_pos, see AMT20 data sheet for process description
+                //0x70: set_zero
+                int command = 0x10; //starting command (if encoder just powerd up start by running no_ops to make the buffer behave, change command to what you want on second run)
+                int commSets = 5;  //how many times to do seperate write/read requests (temporay variable for testing purpose)
 
-                //Display the bytes written
-                Console.WriteLine("");
-                for (int i = 0; i < numBytes; i++)
+                for (int y = 0; y < commSets; y++)
                 {
-                    Console.Out.WriteLine("dataWrite[" + i + "] = " + aBytes[i]);
-                }
+                    //Write the bytes
+                    aBytes[0] = Convert.ToByte(command);
+                    LJM.eWriteNameByteArray(handle, "SPI_DATA_TX", numBytes, aBytes, ref errAddr);
+                    LJM.eWriteName(handle, "SPI_GO", 1);  //Do the SPI communications
+                    //Display the bytes written
+                    Console.WriteLine("");
+                    Console.Out.WriteLine("dataWrite[" + y + "] = " + aBytes[0]);
 
-                //Read the bytes
-                //Initialize byte array values to zero
-                for (int i = 0; i < numBytes; i++)
-                {
-                    aBytes[i] = 0;
-                }
-                LJM.eReadNameByteArray(handle, "SPI_DATA_RX", numBytes, aBytes, ref errAddr);
-                LJM.eWriteName(handle, "SPI_GO", 1);  //Do the SPI communications
 
-                //Display the bytes read
-                Console.Out.WriteLine("");
-                for (int i = 0; i < numBytes; i++)
-                {
-                    Console.Out.WriteLine("dataRead[" + i + "] = " + aBytes[i]);
+                    //Read the bytes
+                    //Initialize byte array values to zero
+                    aBytes[0] = 0;
+                    LJM.eReadNameByteArray(handle, "SPI_DATA_RX", numBytes, aBytes, ref errAddr);
+                    LJM.eWriteName(handle, "SPI_GO", 1);  //Do the SPI communications
+                    //Display the bytes read
+                    Console.Out.WriteLine("");
+                    Console.Out.WriteLine("dataRead[" + y + "] = " + aBytes[0]);
+
+                    command = 0x00; //after intial command is sent we will only want to send no_ops
+                
+                    //wait for user key before next set
+                    Console.Out.WriteLine("");
+                    Console.WriteLine("Press any key to continue on to next set...");
+                    Console.ReadKey();
+
                 }
+                    
+       
             }
             catch (LJM.LJMException ljme)
             {
